6ebfdc1572c15bddcd14397306b0b2e767dbb753
PolpOnline <git@t0mmy.anonaddy.com>
2024-06-22 16:37:02 +0200

    Add favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
 	import '../app.pcss';
 	import 'unfonts.css';
+
+	import favicon from '$lib/img/favicon.svg';
 </script>
+
+<svelte:head>
+	<link rel="icon" type="image/svg+xml" href={favicon} />
+</svelte:head>
 
 <main>
 	<slot></slot>
