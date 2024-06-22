b79b12b83b287fcd197acd557dd398008762582a
PolpOnline <git@t0mmy.anonaddy.com>
2024-06-22 15:51:29 +0200

    Animated logo

--- src/lib/img/PolpOnlineLogo.svelte
+++ src/lib/img/PolpOnlineLogo.svelte
@@ -11,16 +11,88 @@
 	fill="none"
 	class={className}
 >
-	<g clip-path="url(#clip0_1_10)">
-		<rect width="10000" height="10000" fill="#FF0000" rx="1" />
-		<rect x="1" width="10000" height="10000" fill="#DCD2D3" />
-		<rect x="3469.38" y="6409.29" width="3031.25" height="340.708" fill="#372B2B" />
-		<ellipse cx="2641.25" cy="3908.19" rx="656.25" ry="658.186" fill="#372B2B" />
-		<ellipse cx="7360" cy="3923.67" rx="656.25" ry="658.186" fill="#372B2B" />
+	<g id="PolpOnline logo">
+		<rect width="10000" height="10000" fill="#DCD2D3" />
+		<rect
+			id="mouth"
+			x="3469.38"
+			y="6409.29"
+			width="3031.25"
+			height="340.708"
+			fill="#372B2B"
+			stroke="#372B2B"
+			stroke-width="30"
+			stroke-dasharray="6743.916015625"
+			stroke-linecap="round"
+		/>
+		<g id="eyes">
+			<circle
+				id="left"
+				cx="2640"
+				cy="3905"
+				r="645"
+				fill="transparent"
+				stroke="#372B2B"
+				stroke-width="30"
+				stroke-dasharray="4052.24365234375"
+				stroke-linecap="round"
+			/>
+			<circle
+				id="right"
+				cx="7361"
+				cy="3910"
+				r="645"
+				fill="transparent"
+				stroke="#372B2B"
+				stroke-width="30"
+				stroke-dasharray="4052.24365234375"
+				stroke-linecap="round"
+			/>
+		</g>
 	</g>
-	<defs>
-		<clipPath id="clip0_1_10">
-			<rect width="10000" height="10000" fill="white" />
-		</clipPath>
-	</defs>
 </svg>
+
+<style lang="postcss">
+	:root {
+		--animation-duration: 2s;
+	}
+
+	@keyframes draw-eyes {
+		0% {
+			stroke-dashoffset: 4052.24365234375;
+		}
+		50% {
+			stroke-dashoffset: 0;
+		}
+		50.01% {
+			fill: transparent;
+		}
+		100% {
+			fill: #372b2b;
+		}
+	}
+
+	#left,
+	#right {
+		animation: draw-eyes var(--animation-duration) forwards;
+	}
+
+	@keyframes draw-mouth {
+		0% {
+			stroke-dashoffset: 6743.916015625;
+		}
+		50% {
+			stroke-dashoffset: 0;
+		}
+		50.01% {
+			fill: transparent;
+		}
+		100% {
+			fill: #372b2b;
+		}
+	}
+
+	#mouth {
+		animation: draw-mouth var(--animation-duration) forwards;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,13 +3,10 @@
 </script>
 
 <div class="flex h-dvh flex-col items-center justify-center">
-	<PolpOnlineLogo class="color-drop-shadow mb-4 h-[200px] w-[200px] rounded-full" />
+	<PolpOnlineLogo class="img-drop-shadow mb-4 h-[200px] w-[200px] rounded-full" />
 
 	<h1 class="mt-4 text-[3rem]">PolpOnline</h1>
 </div>
 
 <style lang="postcss">
-	.color-drop-shadow {
-		filter: drop-shadow(0 0 1rem #dcd2d3);
-	}
 </style>
