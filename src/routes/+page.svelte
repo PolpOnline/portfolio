7ca52d8dd51939e6195f11827681919137a96bae
PolpOnline <git@t0mmy.anonaddy.com>
2025-03-18 19:02:10 +0100

    Svh

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import { PolpOnlineLogo } from '$lib/img';
 </script>
 
-<div class="flex h-dvh flex-col items-center justify-center">
+<div class="flex h-svh flex-col items-center justify-center">
 	<PolpOnlineLogo class="mb-4 h-[200px] w-[200px] rounded-full" />
 
 	<h1 class="mt-4 text-[3rem]">PolpOnline</h1>
