8b188baa183253492d9be1f9b8bcc63912c5c153
PolpOnline <git@t0mmy.anonaddy.com>
2025-03-18 21:10:16 +0100

    Add social buttons

---
+++ src/lib/components/SocialButtons.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import SkillIconsGithubDark from '~icons/skill-icons/github-dark';
+	import SkillIconsDiscord from '~icons/skill-icons/discord';
+	import LogosTelegram from '~icons/logos/telegram';
+	import SkillIconsInstagram from '~icons/skill-icons/instagram';
+</script>
+
+<div class="flex flex-row items-center justify-center gap-6 text-4xl">
+	<a href="https://github.com/PolpOnline" target="_blank">
+		<SkillIconsGithubDark />
+	</a>
+	<a href="https://telegram.me/PolpOnline" target="_blank">
+		<LogosTelegram />
+	</a>
+	<a href="https://discordapp.com/users/524254508151209995" target="_blank">
+		<SkillIconsDiscord />
+	</a>
+	<a href="https://instagram.com/tommaso.melacarne" target="_blank">
+		<SkillIconsInstagram />
+	</a>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,17 @@
 	import { PolpOnlineLogo } from '$lib/img';
 	import SkillSet from '$lib/components/SkillSet.svelte';
 	import Scroller from '$lib/components/Scroller.svelte';
+	import SocialButtons from '$lib/components/SocialButtons.svelte';
 </script>
 
 <div class="flex h-svh flex-col items-center justify-center">
 	<PolpOnlineLogo class="mb-4 h-[200px] w-[200px] rounded-full" />
 
-	<h1 class="mt-4 mb-20 text-[3rem]">PolpOnline</h1>
+	<h1 class="mt-4 mb-4 text-[3rem]">PolpOnline</h1>
+
+	<div class="mb-20">
+		<SocialButtons />
+	</div>
 
 	<div class="absolute bottom-20 flex flex-col justify-end justify-self-center">
 		<Scroller />
