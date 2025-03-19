0e6a64a49586cd25661d8ec43193f4ff9090ccb6
PolpOnline <git@t0mmy.anonaddy.com>
2025-03-19 19:21:32 +0100

    Fix scroller color

--- src/lib/components/Scroller.svelte
+++ src/lib/components/Scroller.svelte
@@ -7,13 +7,19 @@
 	stroke-linecap="round"
 	stroke-miterlimit="1.5"
 >
-	<path id="wheel" d="M123.359,79.775l0,72.843" fill="none" stroke-width="20px" stroke="#fff" />
+	<path
+		id="wheel"
+		d="M123.359,79.775l0,72.843"
+		fill="none"
+		stroke-width="20px"
+		stroke="currentColor"
+	/>
 	<path
 		id="mouse"
 		d="M236.717,123.359c0,-62.565 -50.794,-113.359 -113.358,-113.359c-62.565,0 -113.359,50.794 -113.359,113.359l0,143.237c0,62.565 50.794,113.359 113.359,113.359c62.564,0 113.358,-50.794 113.358,-113.359l0,-143.237Z"
 		fill="none"
 		stroke-width="20px"
-		stroke="#fff"
+		stroke="currentColor"
 	/>
 </svg>
 
