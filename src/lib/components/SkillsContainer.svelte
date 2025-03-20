aad6f7c04ace322124fba6f0171fa7ad27800776
PolpOnline <git@t0mmy.anonaddy.com>
2025-03-21 00:59:55 +0100

    Useless class

--- src/lib/components/SkillsContainer.svelte
+++ src/lib/components/SkillsContainer.svelte
@@ -12,7 +12,7 @@
 	<h3 class="my-8 font-medium">{title}</h3>
 
 	<div
-		class="skills-container flex w-full flex-wrap justify-center
+		class="flex w-full flex-wrap justify-center
 		gap-4 px-8 text-6xl 2xl:px-14 [&>button]:cursor-default [&>button]:transition-all
 		[&>button]:duration-300 [&>button]:ease-in-out [&>button]:hover:scale-110
 		[&>button:nth-of-type(even)]:hover:-rotate-10 [&>button:nth-of-type(odd)]:hover:rotate-10"
